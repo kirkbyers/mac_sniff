b702730f11116e6c6abc15484b8a63934d79e16a
kirkbyers <kirklbyers@gmail.com>
2025-03-26 23:22:41 -0500

    Working mac collection.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::prelude::Peripherals, nvs::EspDefaultNvsPartition, sys::EspError, wifi::{self, AccessPointConfiguration, BlockingWifi, ClientConfiguration, EspWifi, WifiDeviceId, WifiDriver, WifiFrame}};
+use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::{delay::FreeRtos, prelude::Peripherals}, nvs::EspDefaultNvsPartition, sys::{esp_wifi_set_mode, esp_wifi_set_promiscuous_filter, esp_wifi_set_promiscuous_rx_cb, wifi_mode_t_WIFI_MODE_NULL, wifi_promiscuous_filter_t, WIFI_PROMIS_FILTER_MASK_ALL}, wifi::{self, ClientConfiguration, WifiDriver}};
 use log::info;
 
-const WIFI_SSID: &str = env!("WIFI_SSID");
-const WIFI_PASSWORD: &str = env!("WIFI_PASS");
+const DURRATION_U64: u64 = 30;
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -12,74 +11,89 @@ fn main() -> anyhow::Result<()> {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    info!("Hello, world!");
 
     let peripherals = Peripherals::take()?;
     let sys_loop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
-    let wifi_client_config = ClientConfiguration{
-        ssid: WIFI_SSID.try_into().unwrap(),
-        bssid: None,
-        auth_method: wifi::AuthMethod::WAPIPersonal,
-        password: WIFI_PASSWORD.try_into().unwrap(),
-        channel: None,
-        ..Default::default()
-    };
-    let wifi_ap_config = AccessPointConfiguration {
-        ssid: "ATRIP Rainbows".try_into().unwrap(),
-        ssid_hidden: false,
+    let basic_client_config = ClientConfiguration {
+        ssid: "".try_into().unwrap(),
+        password: "".try_into().unwrap(),
+        channel: Some(1),  // Set to a specific channel
         auth_method: wifi::AuthMethod::None,
         ..Default::default()
     };
-    let wifi_config = wifi::Configuration::Mixed(wifi_client_config, wifi_ap_config);
+    let wifi_config = wifi::Configuration::Client(basic_client_config);
 
     let mut wifi_driver = WifiDriver::new(peripherals.modem, sys_loop.clone(), Some(nvs))?;
 
-    let rx_callback = |_wifi_device_id: WifiDeviceId, wifi_frame: WifiFrame| -> Result<(), EspError> {
-        let frame_data = wifi_frame.as_slice();
-        if frame_data.len() >= 14 {  // Minimum ethernet frame size
-            // MAC addresses
-            let source_mac = &frame_data[6..12];
+    unsafe extern "C" fn rx_callback(buf: *mut core::ffi::c_void, _type: u32) {
+        if !buf.is_null() {
+            let frame_data = std::slice::from_raw_parts(buf as *const u8, 24); // Increased to 24 bytes to capture all MAC addresses
+            let frame_type = (frame_data[0] & 0x0C) >> 2;
+            let frame_subtype = (frame_data[0] & 0xF0) >> 4;
             
-            info!("Source MAC: {:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}", 
-                source_mac[0], source_mac[1], source_mac[2], source_mac[3], source_mac[4], source_mac[5]);
-            
+            let type_str = match frame_type {
+                0 => "Management",
+                1 => "Control",
+                2 => "Data",
+                3 => "Extension",
+                _ => "Unknown"
+            };
+    
+            // Extract MAC addresses
+            let destination_mac = &frame_data[4..10];
+            let source_mac = &frame_data[10..16];
+            let bssid = &frame_data[16..22];
+    
+            info!("Frame: {} (type: {}, subtype: {})", type_str, frame_type, frame_subtype);
+            // info!("  Destination: {:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}", 
+            //     destination_mac[0], destination_mac[1], destination_mac[2],
+            //     destination_mac[3], destination_mac[4], destination_mac[5]);
+            info!("  Source: {:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
+                source_mac[0], source_mac[1], source_mac[2],
+                source_mac[3], source_mac[4], source_mac[5]);
+            info!("  BSSID: {:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
+                bssid[0], bssid[1], bssid[2],
+                bssid[3], bssid[4], bssid[5]);
         }
-        
-        Ok(())
-    };
-    let tx_callback = |wifi_device_id: WifiDeviceId, _data: &[u8], _status: bool| {
-    };
-
-    wifi_driver.set_callbacks(rx_callback, tx_callback)?;
+    }
 
     wifi_driver.set_configuration(&wifi_config)?;
-    wifi_driver.set_promiscuous(true)?;
+    unsafe { 
+        esp_wifi_set_mode(wifi_mode_t_WIFI_MODE_NULL);
+    };
     wifi_driver.start()?;
+    wifi_driver.set_promiscuous(true)?;
+    unsafe {
+        esp_wifi_set_promiscuous_rx_cb(Some(rx_callback));
+        let filter = wifi_promiscuous_filter_t {
+            filter_mask: WIFI_PROMIS_FILTER_MASK_ALL,
+        };
+        esp_wifi_set_promiscuous_filter(&filter);
+    }
 
     info!("Wifi Started");
 
-    // Note: Don't connect if you're going to scan
-    // wifi_driver.connect()?;
-    // info!("Wifi Connected");
-
-    let (scan_result, _found_aps_count) = wifi_driver.scan_n::<10>()?;
-
-    for ap in scan_result.iter() {
-        info!("Found AP: {:?}", ap);
-    }
-
     // Run for 30 seconds then exit
     let start = std::time::Instant::now();
-    let duration_u64: u64 = 3000;
-    let duration = std::time::Duration::from_secs(duration_u64);
+    let duration = std::time::Duration::from_secs(DURRATION_U64);
+    let mut last_check_in_time = start;
+
     while start.elapsed() < duration {
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        info!("Time remaining: {} seconds", duration_u64 - start.elapsed().as_secs());
+        if last_check_in_time.elapsed() >= std::time::Duration::from_secs(1) {
+            info!("Time remaining: {} seconds", DURRATION_U64 - start.elapsed().as_secs());
+
+            last_check_in_time = std::time::Instant::now();
+        }
+        FreeRtos::delay_ms(50);
     }
 
-    info!("{} seconds elapsed, exiting...", duration_u64);
+    // Cleanup before exit
+    wifi_driver.set_promiscuous(false)?;
+    wifi_driver.stop()?;
+    info!("{} seconds elapsed, exiting...", DURRATION_U64);
 
     Ok(())
 }
